bec864c1117e487280752c54d6ed5fb3061b483c
zjm168love <476789259@qq.com>
2017-12-07 18:02:13 +0800

    整理代码结构

--- src/test/java/springbootelastic/ElasticApplicationTests.java
+++ src/test/java/springbootelastic/ElasticApplicationTests.java
@@ -43,27 +43,50 @@ public class ElasticApplicationTests {
    * 建立索引
    */
   @Test
-  public void testSaveArticleIndex() {
-    articleSearchRepository.refresh();
+  public void createIndex() {
+    elasticsearchTemplate.deleteIndex(Article.class);
+    elasticsearchTemplate.createIndex(Article.class);
+    elasticsearchTemplate.putMapping(Article.class);
+    elasticsearchTemplate.refresh(Article.class);
+  }
 
+  /**
+   * 删除索引
+   */
+  @Test
+  public void deleteIndex() {
+    elasticsearchTemplate.deleteIndex(Article.class);
+  }
+
+  /**
+   * 添加data
+   */
+  @Test
+  public void testSaveArticleIndex() {
     Author author = new Author();
     author.setId(1L);
-    author.setName("tianshouzhi");
+    author.setName("狂扁小朋友");
     author.setRemark("java developer");
 
-    Article article = new Article();
-    article.setId(1L);
-    article.setTitle("springboot integreate elasticsearch");
-    article.setAbstracts("springboot integreate elasticsearch is very easy");
-    article.setAuthor(author);
-    article.setContent("elasticsearch based on lucene,"
-      + "spring-data-elastichsearch based on elaticsearch"
-      + ",this tutorial tell you how to integrete springboot with spring-data-elasticsearch");
-    article.setPostTime(new Date());
-    article.setClickCount(1L);
+    List<Article> articleList = new ArrayList<>();
+    articleList.add(new Article(1L, 11, "es1", "elasticsearch is very easy",
+      "elasticsearch based on lucene", new Date(), 2L, author));
+    articleList.add(new Article(2L, 22, "es1", "elasticsearch is amazing",
+      "you can enjoy it", new Date(), 1L, author));
+    articleList.add(new Article(3L, 33, "es1", "the release of elasticsearch ",
+      "elasticsearch make somebody unhappy", new Date(), 5L, author));
+    articleList.add(new Article(4L, 44, "es4", "水电费得到",
+      "未入围欧特克框架", new Date(), 0L, author));
+    articleList.add(new Article(5L, 55, "es5", "是否时代峻峰呼市多久",
+      "鹿盔胜多负少的计划", new Date(), 90L, author));
+    articleList.add(new Article(6L, 66, "es6", "二恶而",
+      "让他有图有认同与他人", new Date(), 11L, author));
+    articleList.add(new Article(7L, 77, "es7", "的范德萨发",
+      "手动阀十大歌手发过火根据返回", new Date(), 10L, author));
 
-    articleSearchRepository.save(article);
+    articleSearchRepository.save(articleList);
   }
+
 
   /**
    * 对所有field都进行搜索search
@@ -71,7 +94,7 @@ public class ElasticApplicationTests {
   @Test
   public void testSearch() {
 
-    String queryString = "的";//搜索关键字
+    String queryString = "的";
     QueryStringQueryBuilder builder = new QueryStringQueryBuilder(queryString);
     Iterable<Article> searchResult = articleSearchRepository.search(builder);
     System.out.println("遍历结果------------------------------");
@@ -136,6 +159,9 @@ public class ElasticApplicationTests {
     }
   }
 
+  /**
+   * 分词测试
+   */
   @Test
   public void testIKAnalyze() {
     List<String> searchTermsList = getIkAnalyzeSearchTerms("鹿盔胜多负少");
